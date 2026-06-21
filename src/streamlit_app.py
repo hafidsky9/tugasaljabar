f9fb7e1980b9b250f184f57c70954e4eabcd2bfd
hafidsky9         git config --global user.email hafidfirmansyah070706@gmail.comgit config --global user.name hafidsky9 <emailgithubkamu@gmail.com>
2026-06-22 00:51:40 +0700

    fix pca compression

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -233,7 +233,7 @@ elif menu == "Kompresi Gambar PCA":
 
             with colB:
                 st.metric(
-                    "Ukuran kompres",
+                    "Ukuran kompresi",
                     f"{compressed_size:.2f}kb "
                 )
 
